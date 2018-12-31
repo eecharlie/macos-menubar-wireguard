97c226ee024797f7b6571b9ab802d3ab8d19bd9b
Charlie Matlack (MBP) <charlie@potavida.org>
2018-12-31 09:53:27 -0800

    streamline build setup

--- Misc/build-setup.sh
+++ Misc/build-setup.sh
@@ -10,11 +10,17 @@ die() {
 
 [[ ${BASH_VERSINFO[0]} -ge 4 ]] || die "bash ${BASH_VERSINFO[0]} detected, when bash 4+ required"
 
-# TODO the conditional logic here doesn't work as desired
+# Ensure XCode and Homebrew are installed
+type xcodebuild >/dev/null || echo "You need to install XCode from the Apple Store"
+type brew >/dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || die "Failed to install Homebrew"
 
-# unlink imagemagick if already installed & wrong version
-convert --version | grep 7.0.8-9 || brew unlink imagemagick
+# Invoke Brewfile in top-level dir to install dependencies available from Homebrew
+cd "${0%/*}/../" && brew bundle
 
-# install older version using hard link to git commit
+# Imagemagick 7.0.8-9 must be used to avoid bug in floodfill. Homebrew can be brute-forced to specific version:
+# 1) unlink imagemagick if already installed & wrong version
+type convert >/dev/null || convert --version | grep 7.0.8-9 || brew unlink imagemagick
+# 2) install older version using hard link to git commit
 brew info imagemagick | grep 7.0.8-9 || brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/17141e5dce0c42f9490467ed9eb204fd8e178f4c/Formula/imagemagick.rb
-brew link imagemagick
+# 3) link if installed and not linked
+convert --version | grep 7.0.8-9 || brew link imagemagick
